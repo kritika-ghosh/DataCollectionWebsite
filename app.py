0579adb2b22bd32dea91ef18e237410e7ef1f0e7
kritika-ghosh <kri.kritika.ghosh@gmail.com>
2025-08-24 19:58:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -718,4 +718,6 @@ with gr.Blocks() as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=10000)
+    pass
+    #demo.launch(server_name="0.0.0.0", server_port=10000)
+
