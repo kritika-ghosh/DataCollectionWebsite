ac1f3102fdd016f7f0d1241e54eed972d3d723b2
kritika-ghosh <kri.kritika.ghosh@gmail.com>
2025-08-24 19:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -715,6 +715,3 @@ with gr.Blocks() as demo:
         fn=save_all_data_to_gsheet, # Changed to use the Google Sheet saving function
         outputs=final_status
     )
-
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=10000)
